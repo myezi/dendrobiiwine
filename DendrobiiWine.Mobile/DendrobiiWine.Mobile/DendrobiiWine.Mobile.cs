388f1246bfe4303fd472ef7c58754078cd9f43a2
赵旭明 <hhappyzxm@outlook.com>
2015-11-24 14:27:59 +0800

    Temp submit.

--- DendrobiiWine.Mobile/DendrobiiWine.Mobile/DendrobiiWine.Mobile.cs
+++ DendrobiiWine.Mobile/DendrobiiWine.Mobile/DendrobiiWine.Mobile.cs
@@ -8,18 +8,8 @@ namespace DendrobiiWine.Mobile
 	{
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+            // The root page of your application
+            MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart ()
---
+++ DendrobiiWine.Mobile/DendrobiiWine.Mobile/Views/MerchantListPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DendrobiiWine.Mobile.Views
+{
+    public partial class MerchantListPage : ContentPage
+    {
+        public MerchantListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
