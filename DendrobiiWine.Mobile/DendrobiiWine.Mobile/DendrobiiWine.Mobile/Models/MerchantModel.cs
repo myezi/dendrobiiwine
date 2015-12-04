def02685e3ac664bbe10f934ecfdc212f9d99176
赵旭明 <hhappyzxm@outlook.com>
2015-12-04 16:48:35 +0800

    Temp commit.

--- DendrobiiWine.Mobile/DendrobiiWine.Mobile/DendrobiiWine.Mobile/Models/MerchantModel.cs
+++ DendrobiiWine.Mobile/DendrobiiWine.Mobile/DendrobiiWine.Mobile/Models/MerchantModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace DendrobiiWine.Mobile.Models
 {
-    class MerchantModel
+    public class MerchantModel
     {
+        public ImageSource Image { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Price { get; set; }
     }
 }
--- DendrobiiWine.Mobile/DendrobiiWine.Mobile/DendrobiiWine.Mobile/Pages/Customer/CustomerMainPage.xaml.cs
+++ DendrobiiWine.Mobile/DendrobiiWine.Mobile/DendrobiiWine.Mobile/Pages/Customer/CustomerMainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DendrobiiWine.Mobile.App_Code;
+using DendrobiiWine.Mobile.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +15,85 @@ namespace DendrobiiWine.Mobile.Pages.Customer
         public CustomerMainPage()
         {
             InitializeComponent();
-        }
 
+            listView.ItemsSource = new List<MerchantModel>
+            {
+                new MerchantModel {
+                    Image = ImageSource.FromUri(new Uri("http://avatar.csdn.net/9/A/4/1_tangcheng_ok.jpg")),
+                    Name = "马山牛奶",
+                    Description = "[东亭]鲜牛奶30杯每天送 不限时间段通用",
+                    Price = "80.00"
+                },
+                new MerchantModel {
+                    Image = ImageSource.FromUri(new Uri("http://avatar.csdn.net/9/A/4/1_tangcheng_ok.jpg")),
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Image = ImageSource.FromUri(new Uri("http://avatar.csdn.net/9/A/4/1_tangcheng_ok.jpg")),
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Image = ImageSource.FromUri(new Uri("http://avatar.csdn.net/9/A/4/1_tangcheng_ok.jpg")),
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+                new MerchantModel {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = DateTime.Now.ToString()
+                },
+            };
+        }
+        
         private async void OnCityClick(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new CityListPage());
